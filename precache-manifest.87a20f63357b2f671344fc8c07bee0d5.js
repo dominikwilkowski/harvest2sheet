757dce82fffa18022c3bda36beb228c9809d8df0
Dominik Wilkowski <hi@dominik-wilkowski.com>
2020-09-20 16:09:57 +1000

    Updates

---
+++ precache-manifest.87a20f63357b2f671344fc8c07bee0d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf5b5f7c583efb3a13723246fa473ca",
+    "url": "/harvest2sheet/index.html"
+  },
+  {
+    "revision": "35aff9011fd42d044266",
+    "url": "/harvest2sheet/static/js/2.f22636ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/harvest2sheet/static/js/2.f22636ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c0eeede6de365c4fa0",
+    "url": "/harvest2sheet/static/js/main.375fb74a.chunk.js"
+  },
+  {
+    "revision": "a6626191534e996adadb",
+    "url": "/harvest2sheet/static/js/runtime-main.a4e55032.js"
+  }
+]);
