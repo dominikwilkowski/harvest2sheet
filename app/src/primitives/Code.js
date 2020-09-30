69fb7aabf9511d1af872a0a14099ff86ae521071
Dominik Wilkowski <hi@dominik-wilkowski.com>
2020-09-30 15:14:41 +1000

    Added client and project prefix for code boxes

--- app/src/primitives/Code.js
+++ app/src/primitives/Code.js
@@ -2,9 +2,21 @@
 
 import { jsx } from '@emotion/core';
 
-export function Code(props) {
+export function Code({ kind, ...props }) {
+	const kindMap = {
+		project: {
+			content: '"P"',
+			title: 'Project',
+		},
+		client: {
+			content: '"C"',
+			title: 'Client',
+		},
+	};
+
 	return (
 		<span
+			title={kind ? kindMap[kind].title : null}
 			css={{
 				display: 'inline-block',
 				background: 'var(--alt-bg)',
@@ -13,6 +25,18 @@ export function Code(props) {
 				borderRadius: '3px',
 				margin: '0.25rem 0.25rem 0 0',
 				fontSize: '0.75rem',
+				...(kind
+					? {
+							':before': {
+								content: kindMap[kind].content,
+								display: 'inline-block',
+								fontWeight: 900,
+								marginRight: '0.25rem',
+								paddingRight: '0.25rem',
+								borderRight: '1px solid #ccc',
+							},
+					  }
+					: {}),
 			}}
 			{...props}
 		/>
